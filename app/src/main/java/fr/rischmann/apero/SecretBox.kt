6aa9b906868f2f052337ebbe9f0ada88d0ed43a5
Vincent Rischmann <vincent@rischmann.fr>
2019-09-03 00:08:19 +0200

    add an implementation of SecretBox

---
+++ app/src/main/java/fr/rischmann/apero/SecretBox.kt
@@ -0,0 +1,103 @@
+package fr.rischmann.apero
+
+import org.bouncycastle.crypto.engines.XSalsa20Engine
+import org.bouncycastle.crypto.macs.Poly1305
+import org.bouncycastle.crypto.params.KeyParameter
+import org.bouncycastle.crypto.params.ParametersWithIV
+import java.security.MessageDigest
+import java.security.SecureRandom
+import java.util.*
+import kotlin.math.max
+import kotlin.math.min
+
+// SecretBox is a compatible implementation of https://godoc.org/golang.org/x/crypto/nacl/secretbox
+// which is in turn a compatible implementation of https://nacl.cr.yp.to/secretbox.html
+//
+// I based this code on https://github.com/codahale/xsalsa20poly1305/blob/master/src/main/java/com/codahale/xsalsa20poly1305/SecretBox.java
+internal class SecretBox(key: ByteArray) {
+    private val key: ByteArray
+
+    init {
+        Objects.requireNonNull(key)
+        require(key.size == KEY_SIZE) { "key must be 32 bytes" }
+        this.key = key.copyOf(key.size)
+
+    }
+
+    fun open(ciphertext: ByteArray, nonce: ByteArray): ByteArray? {
+        val xSalsa20 = XSalsa20Engine()
+        val poly1305 = Poly1305()
+
+        // initialize XSalsa20
+        xSalsa20.init(true, ParametersWithIV(KeyParameter(key), nonce))
+
+        // generate Poly1305 sub key
+        val subKey = ByteArray(32)
+        xSalsa20.processBytes(subKey, 0, subKey.size, subKey, 0)
+
+        // hash ciphertext
+        poly1305.init(KeyParameter(subKey))
+        val len = max(ciphertext.size - poly1305.macSize, 0)
+        poly1305.update(ciphertext, poly1305.macSize, len)
+        val calculatedMac = ByteArray(poly1305.macSize)
+        poly1305.doFinal(calculatedMac, 0)
+
+        val presentedMac = ByteArray(poly1305.macSize)
+        System.arraycopy(ciphertext, 0, presentedMac, 0, min(ciphertext.size, poly1305.macSize))
+
+        if (!MessageDigest.isEqual(calculatedMac, presentedMac)) {
+            return null
+        }
+
+        val plaintext = ByteArray(len)
+        xSalsa20.processBytes(ciphertext, poly1305.macSize, plaintext.size, plaintext, 0)
+
+        return plaintext
+    }
+
+    fun seal(message: ByteArray, nonce: ByteArray): ByteArray {
+        val xSalsa20 = XSalsa20Engine()
+        val poly1305 = Poly1305()
+
+        // initialize XSalsa20
+        xSalsa20.init(true, ParametersWithIV(KeyParameter(key), nonce))
+
+        // generate Poly1305 sub key
+        val subKey = ByteArray(32)
+        xSalsa20.processBytes(subKey, 0, subKey.size, subKey, 0)
+
+        // encrypt plaintext
+        val out = ByteArray(message.size + poly1305.macSize)
+        xSalsa20.processBytes(message, 0, message.size, out, poly1305.macSize)
+
+        // hash ciphertext and prepend MAC to ciphertext
+        poly1305.init(KeyParameter(subKey))
+        poly1305.update(out, poly1305.macSize, message.size)
+        poly1305.doFinal(out, 0)
+
+        return out
+    }
+
+    companion object {
+        const val NONCE_SIZE = 24
+        const val KEY_SIZE = 32
+
+        fun newNonce(): ByteArray {
+            val key = ByteArray(NONCE_SIZE)
+
+            val secureRandom = SecureRandom()
+            secureRandom.nextBytes(key)
+
+            return key
+        }
+
+        fun newKey(): ByteArray {
+            val key = ByteArray(KEY_SIZE)
+
+            val secureRandom = SecureRandom()
+            secureRandom.nextBytes(key)
+
+            return key
+        }
+    }
+}
---
+++ app/src/test/java/fr/rischmann/apero/SecretBoxTest.kt
@@ -0,0 +1,43 @@
+package fr.rischmann.apero
+
+import junit.framework.Assert.assertEquals
+import org.bouncycastle.util.encoders.Hex
+import org.junit.Assert.assertArrayEquals
+import org.junit.Test
+
+class SecretBoxTest {
+    @Test
+    fun sealOpenGoGenerated() {
+        val key = Hex.decode("e39616fa92ab3779f1503a31ed71a9990eb3524be51257b7d645ec588dc0b7e1")
+
+        val input =
+            Hex.decode("43ef80e62ee631cf73cf526ccff1dc92c4cbdc1c4b14c19a46cc2b612ed698e8fbc9f34caa3fecb50673200ede82")
+
+        val nonce = input.sliceArray(0 until SecretBox.NONCE_SIZE)
+        val ciphertext = input.sliceArray(SecretBox.NONCE_SIZE until input.size)
+
+        val box = SecretBox(key)
+        val plaintext = box.open(ciphertext, nonce)
+
+        val message = plaintext?.toString(Charsets.UTF_8)
+        assertEquals("plaintext is invalid", "yellow", message)
+    }
+
+    @Test
+    fun sealOpen() {
+        val key = SecretBox.newKey()
+        val box = SecretBox(key)
+        val nonce = SecretBox.newNonce()
+
+        val message = "foobar".toByteArray()
+
+        val ciphertext = box.seal(message, nonce)
+        val plaintext = box.open(ciphertext, nonce)
+
+        assertArrayEquals("decrypted plaintext is invalid", message, plaintext)
+    }
+
+    private fun ByteArray.toHex(): String {
+        return joinToString("") { "%02x".format(it) }
+    }
+}
