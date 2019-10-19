858144415f34f610cbf4ea0f736ae9713875c2bb
Vincent Rischmann <vincent@rischmann.fr>
2019-10-20 01:54:45 +0200

    update

--- app/src/main/java/fr/rischmann/apero/CopyFragment.kt
+++ app/src/main/java/fr/rischmann/apero/CopyFragment.kt
@@ -1,9 +1,12 @@
 package fr.rischmann.apero
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class CopyFragment : Fragment() {
@@ -11,5 +14,15 @@ class CopyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_copy, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val editText = view.findViewById<EditText>(R.id.copyEditText)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
     }
 }
