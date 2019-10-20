c883de40a039a77970cff7db6ff30797abbc9523
Vincent Rischmann <vincent@rischmann.fr>
2019-10-20 20:42:05 +0200

    update

--- app/src/main/java/fr/rischmann/apero/Entry.kt
+++ app/src/main/java/fr/rischmann/apero/Entry.kt
@@ -1,3 +1,13 @@
 package fr.rischmann.apero
 
-data class Entry(val id: ULID, val content: String)
+import java.security.SecureRandom
+import java.time.Instant
+
+data class Entry(val id: ULID, val content: String) {
+    companion object {
+        public final val ALL = listOf(
+            Entry(ULID.random(Instant.now().toEpochMilli(), SecureRandom()), "foobar"),
+            Entry(ULID.random(Instant.now().toEpochMilli(), SecureRandom()), "barbaz")
+        )
+    }
+}
