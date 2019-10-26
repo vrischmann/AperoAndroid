179bf7f10295dbe405901c9bc657d01ca84e13f5
Vincent Rischmann <vincent@rischmann.fr>
2019-10-27 01:51:07 +0200

    secret box: fix test

--- app/src/test/java/fr/rischmann/apero/SecretBoxTest.kt
+++ app/src/test/java/fr/rischmann/apero/SecretBoxTest.kt
@@ -10,16 +10,18 @@ class SecretBoxTest {
     @Test
     fun sealOpenGoGenerated() {
         val key = Hex.decode("61caae80b04a17c12a824c6e65ff7e65da611b23e2aa0a4432995e8be79dc2f2")
-        val input = Hex.decode("1de6a57f064df85a81620a925eb85adb04fce5ad12aed3e590fb0d7d856a5bcec6590c128468ceff8c35")
+        val input = Hex.decode("07540d0ad584eb29b01b8dad06309ea4604dc19b4d3bb02017d3967476922433c0bdb5b3c549cac1d57015770cf6a6")
 
         val nonce = input.sliceArray(0 until SecretBox.NONCE_SIZE)
+        assertTrue(nonce.size == SecretBox.NONCE_SIZE)
         val ciphertext = input.sliceArray(SecretBox.NONCE_SIZE until input.size)
+        assertTrue(ciphertext.isNotEmpty())
 
         val box = SecretBox(key)
         val plaintext = box.open(ciphertext, nonce)
 
         val message = plaintext?.toString(Charsets.UTF_8)
-        assertEquals("plaintext is invalid", "yellow", message)
+        assertEquals("plaintext is invalid", "yellow\n", message)
     }
 
     @Test
